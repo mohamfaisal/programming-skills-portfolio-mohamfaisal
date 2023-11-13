4c9058bfa82058cda8d32d9ff6bc5a793dce0b54
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-12 22:07:34 -0800

    Exercises chap 6

---
+++ Chapter 6- Loops/Exercises chap 6/exercise3.py
@@ -0,0 +1,4 @@
+# Write a loop that never ends, and run it. 
+# (To end the loop, press ctrl-C or close the window displaying the output.)
+while True:
+    print("This loop will run forever. You can end it by pressing Ctrl-C or close the Window displaying the output ( Terminal ) :)")

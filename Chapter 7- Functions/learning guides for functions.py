84388bc38470564a6dc5dad2014383f6c3a969f0
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 10:45:54 -0800

    Learning Material

---
+++ Chapter 7- Functions/learning guides for functions.py
@@ -0,0 +1,30 @@
+# Void function 
+def print_message():
+    print("Hello Students")
+# Calling function 
+print_message()
+
+# Local variable 
+def print_message():
+    message = "Hello Students"   # Local variable 
+    print(message)
+# Calling Function 
+print_message()
+
+# If i call this message it shows an error 
+
+def print_message():
+    message = "Hello Students"   # Local variable 
+    print(message)
+print(message)      # message is not defined because local variable life is inside of program
+print_message()
+
+# Different functions have same local variables name - No syntax error 
+
+def print_message():
+    message = "hy"
+    print(message)
+
+def print_message_2():
+    message ="hello"
+    print(message)

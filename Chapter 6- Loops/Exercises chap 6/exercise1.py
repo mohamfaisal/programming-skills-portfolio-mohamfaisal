1d2cd862df56f5e411ab2b42663112feafa1fbff
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 09:59:05 -0800

    exercise 1

--- Chapter 6- Loops/Exercises chap 6/exercise1.py
+++ Chapter 6- Loops/Exercises chap 6/exercise1.py
@@ -1,5 +1,5 @@
-#Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
-#As they enter each topping, print a message saying you’ll add that topping to their pizza.
+# Write a loop that prompts the user to enter a series of pizza toppings until they enter a 'quit' value.
+# As they enter each topping, print a message saying you’ll add that topping to their pizza.
 
 while True:
     toppings=input('Enter the topping:(or type quit to finish:)' )

a95b34bc8830c971615dd94f7e6520a349635920
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 09:59:54 -0800

    exercise 2

--- Chapter 6- Loops/Exercises chap 6/exercise2.py
+++ Chapter 6- Loops/Exercises chap 6/exercise2.py
@@ -1,7 +1,7 @@
-#A movie theater charges different ticket prices depending on a person’s age.
-#If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10;
-#and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, 
-#and then tell them the cost of their movie ticket
+# A movie theater charges different ticket prices depending on a person’s age.
+# If a person is under the age of 3, the ticket is free; if they are between 3 and 12, the ticket is $10;
+# and if they are over age 12, the ticket is $15. Write a loop in which you ask users their age, 
+# and then tell them the cost of their movie ticket
 
 while True:
     age=input('Enter your age:')

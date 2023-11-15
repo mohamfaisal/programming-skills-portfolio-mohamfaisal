2d2aea0289173065fb912b4641ea9f31d3248c1c
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 17:20:50 -0800

    excercise 4

--- Chapter 1- Getting Started with Python/Exercises/excercise4.py
+++ Chapter 1- Getting Started with Python/Exercises/excercise4.py
@@ -1,6 +1,8 @@
+# Write three strings in different variables and print the output in one string.
+
 string1 = "My "
 string2 = "name is "
-string3 = "Mohammed Faisal "
+string3 = "Mohammed Faisal"
 total_string = string1 + string2 + string3
 print(total_string)
 

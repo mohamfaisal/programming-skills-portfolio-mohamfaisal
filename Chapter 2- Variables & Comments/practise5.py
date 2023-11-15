b7eb2771487c31969fd23de5fa4e7e89118f96ec
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 18:16:40 -0800

    tgf

--- Chapter 2- Variables & Comments/practise5.py
+++ Chapter 2- Variables & Comments/practise5.py
@@ -1,2 +1,2 @@
-# Write a python program that stores fruit names in a list named fruits. 
-# Unpack the list into three variables x, y and z and then print the values of each variable.
+#Write a python program that stores fruit names in a list named fruits. 
+#Unpack the list into three variables x, y and z and then print the values of each variable.

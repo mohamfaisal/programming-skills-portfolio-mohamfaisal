23081545887326caacdba3d15544489505769b4a
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 22:26:05 -0800

    practise 4

---
+++ Chapter 3- Data Structures/Practises/practise4.py
@@ -0,0 +1,8 @@
+# Assume the lists numbers1 has 100 elements and numbers2 is an empty list. 
+# Write code that copies the values in numbers to numbers2.
+
+
+List = [i for i in range(1, 101)]  
+numbers2 = []
+numbers2 = List.copy()
+print("numbers2:", numbers2)

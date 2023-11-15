47e102bee5f3cd847ca3ffa70d643c7d2a3c24f2
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 18:04:16 -0800

    practise 1

---
+++ Chapter 2- Variables & Comments/practkmk1.py
@@ -0,0 +1,2 @@
+# Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. Add all the
+# variables and then store the result in variable a. Print the value of a. 

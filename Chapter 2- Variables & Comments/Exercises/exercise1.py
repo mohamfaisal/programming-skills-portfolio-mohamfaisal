c5fad2175e0f01e2466fba96c3171e95da8d1cc5
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:36:04 -0800

    exercise 1

---
+++ Chapter 2- Variables & Comments/Exercises/exercise1.py
@@ -0,0 +1,8 @@
+#Assign a message to a variable, and print that message,
+#Then change the value of the variable to a new message, and print the new message. 
+
+message = "Hello"
+print(message)
+#Then change the value of the variable to a new message, and print the new message.
+message2 ="World"
+print(message2)

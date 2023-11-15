ab08ea54c49c3f6c02013c1b0a1f04fcc69e15c0
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 23:31:04 -0800

    practise 5

---
+++ Chapter 5- Dictionaries/Practises/practise5.py
@@ -0,0 +1,5 @@
+# Write a Python program to merge two dictionaries into one.
+
+Dictionary_1 = {"Name": "Faisal", "Student_ID": 2023467, "Module": "Code lab"}
+Dictionary_2 = {"Name": "Adil", "Student_ID": 2023468, "Module": "Code lab"}
+print(Dictionary_1|Dictionary_2)

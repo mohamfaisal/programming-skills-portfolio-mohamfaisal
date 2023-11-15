c2cc1aa6a65691dd8100a9b81e0bbd0604610e03
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-15 13:56:13 -0800

    practise 2

---
+++ Chapter 7- Functions/Practises/practise2.py
@@ -0,0 +1,10 @@
+# Write a Python function that calculates the factorial of a given positive integer using recursion.
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+       
+
+print(factorial(5)) # Output: 120

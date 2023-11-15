9d73395a5c65f98cecb248f9a062a0489ede4792
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-15 12:13:38 -0800

    practise 1

---
+++ Chapter 7- Functions/Practises/practise1.py
@@ -0,0 +1,6 @@
+# Write a Python function that takes two numbers as arguments and returns their sum.
+
+def numbers(number_1, number_2):
+    sum = number_1 + number_2
+    return sum
+print(numbers(4, 2))

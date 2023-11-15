6120341f4bbb8dd22b4a908657f732db6ca0c83c
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:46:08 -0800

    practise 4

--- Chapter 2- Variables & Comments/Practises/practise4.py
+++ Chapter 2- Variables & Comments/Practises/practise4.py
@@ -1,2 +1,10 @@
 # Write a python program that stores an integer and string value to variables x and y. 
 # print the type of variable x and y.
+
+# Storing an integer and string value to variables x and y
+x = 600
+y = "Faisal"
+
+# Printing the type of variable x and y
+print(" variable X is:",type(x))
+print(" variable Y is:",type(y))

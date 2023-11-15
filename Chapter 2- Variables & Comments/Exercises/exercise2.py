8fc2925b6916902e40ee534c008bdd0acdb0a4f7
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:36:11 -0800

    exercise 2

---
+++ Chapter 2- Variables & Comments/Exercises/exercise2.py
@@ -0,0 +1,9 @@
+# Find a quote from a famous person you admire. Print the quote and the name of its author. 
+# Your output should look something like the following, including the quotation marks:
+# Albert Einstein once said, “A person who never made a mistake never tried anything new.
+
+string1 = "Robert Kiyosaki once said,"
+string2 = " “The single most powerful asset we all have is our mind."
+string_output = string1 + string2
+print(string_output)
+

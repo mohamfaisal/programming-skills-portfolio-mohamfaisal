9ee8d5f141454d10e853ff1a1e719188214cb57e
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:54:42 -0800

    exercise 1

---
+++ Chapter 3- Data Structures/Exercises/exercise1.py
@@ -0,0 +1,7 @@
+#Store the names of a few of your friends in a list called names.
+#Print each person’s name by accessing each element in the list, one at a time.
+
+names = ["Adil", "Faisal", "Abdullah", "Rashid", "Abubakkar"]
+print(names)
+friends = names [0:6]
+print(friends)

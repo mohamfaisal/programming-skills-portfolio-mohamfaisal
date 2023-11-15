5844238e8698c2f0b4102c2081dc2016efc20491
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 18:17:54 -0800

    tgf

--- Chapter 2- Variables & Comments/practise3.py
+++ Chapter 2- Variables & Comments/practise3.py
@@ -1,2 +1,2 @@
-#Write a python program that takes an input 5 from user and then type cast those values to string, int
-#and float in the separate variables. Print all the variables
+# Write a python program that takes an input 5 from user and then type cast those values to string, int
+# and float in the separate variables. Print all the variables

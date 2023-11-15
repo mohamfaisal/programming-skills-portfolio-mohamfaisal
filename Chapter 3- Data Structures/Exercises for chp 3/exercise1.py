95cac3de28c54c66d8544e141f880bb8432b65aa
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:53:40 -0800

    e

--- Chapter 3- Data Structures/Exercises for chp 3/exercise1.py
+++
@@ -1,7 +0,0 @@
-#Store the names of a few of your friends in a list called names.
-#Print each person’s name by accessing each element in the list, one at a time.
-
-names = ["Adil", "Faisal", "Abdullah", "Rashid", "Abubakkar"]
-print(names)
-friends = names [0:6]
-print(friends)

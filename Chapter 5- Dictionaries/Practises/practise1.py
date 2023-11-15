320c8c101031b25b1fb7e97815a6068d74fa79f7
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 23:30:28 -0800

    practise 1

---
+++ Chapter 5- Dictionaries/Practises/practise1.py
@@ -0,0 +1,7 @@
+# Use a dictionary to store information about yourself.
+
+Dictionary = {"Name": "Faisal",
+               "Age": 18,
+                 "Nationality": "Indian",
+                   "University": "Bathspa University RAK"}
+print(Dictionary)

67001aa095231b1e9f457007ed2ad18c514e074b
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 23:30:45 -0800

    practise 3

---
+++ Chapter 5- Dictionaries/Practises/practise3.py
@@ -0,0 +1,9 @@
+# Write a Python program to iterate through the values of a dictionary and print them
+
+Dictionary = {"Name": "Faisal",
+               "Age": 18,
+                 "Nationality": "Indian",
+                   "University": "Bathspa University RAK"}
+print(Dictionary)
+for value in Dictionary.values():
+    print(value)

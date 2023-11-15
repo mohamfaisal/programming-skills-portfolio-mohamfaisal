926c38c18c33e9d74ecde4578337ab73d0bd8604
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 18:12:31 -0800

    ze

---
+++ Chapter 2- Variables & Comments/practise2.py
@@ -0,0 +1,3 @@
+# Write a python program that takes courses marks as input and then calculates average of all the courses. 
+# After calculating the average, calculate the percentage of a student using total marks. 
+# Assume the total of all the courses marks is 500 or take the total marks from the user as input. 

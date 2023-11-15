a0c9a7210b5ec0a26ee2b1c499eac673d0fb2304
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-15 10:56:46 -0800

    Learning Material

---
+++ Chapter 2- Variables & Comments/Learning Material/Notes.py
@@ -0,0 +1,35 @@
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+# Creating a comment starts with " # " 
+#This is a comment
+print("Hello, World!")
+
+# Creating Variables 
+x = 5                 # A variable is created the moment you first assign a value to it
+y = "John"
+print(x)
+print(y)
+
+# Casting          # If you want to specify the data type of a variable, this can be done with casting.
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3            
+z = float(3)  # z will be 3.0
+
+# Get the type     # You can get the data type of a variable with the type() function
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+# Single or double qoutes  
+x = "John"                  # String variables can be declared either by using single or double quotes
+# is the same as
+x = 'John'
+
+# Case-sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
+

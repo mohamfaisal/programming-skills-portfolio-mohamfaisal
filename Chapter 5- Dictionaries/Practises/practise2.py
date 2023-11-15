0f1387f246e30b5c5b61b0a9aaff68bc76b1c2d3
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 23:30:37 -0800

    practise 2

---
+++ Chapter 5- Dictionaries/Practises/practise2.py
@@ -0,0 +1,9 @@
+# Write a Python program to iterate through the keys of a dictionary and print them
+
+Dictionary = {"Name": "Faisal",
+               "Age": 18,
+                 "Nationality": "Indian",
+                   "University": "Bathspa University RAK"}
+print(Dictionary)
+for key in Dictionary:
+    print(key)

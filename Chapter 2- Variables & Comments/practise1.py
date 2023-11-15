ffe260e1b47e4869a4059f0b12d5788481b4bcdd
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:37:18 -0800

    ed

--- Chapter 2- Variables & Comments/practise1.py
+++
@@ -1,8 +0,0 @@
-# Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. Add all the
-# variables and then store the result in variable a. Print the value of a. 
-
-X = 10
-Y = 20
-Z = 30
-a = X + Y + Z
-print(a)

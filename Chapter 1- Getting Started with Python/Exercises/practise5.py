ea8374d4577a3ec05c47ea43e30caba7db3b3111
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 21:30:52 -0800

    practise 5

--- Chapter 1- Getting Started with Python/Exercises/practise5.py
+++
@@ -1,8 +0,0 @@
-# Write four strings in different variables and print the output as one string.(string concatenation).
-string1 = "My "
-string2 = "name is "
-string3 = "Mohammed Faisal "
-string4 = "Hello, "
-total_string = string4 + string1 + string2 + string3 
-print(total_string)
-

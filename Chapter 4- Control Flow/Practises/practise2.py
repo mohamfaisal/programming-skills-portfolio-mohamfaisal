931c4a62cf4e4fa337993e37abf6f8ec3f957ca9
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 22:58:17 -0800

    practise 2

---
+++ Chapter 4- Control Flow/Practises/practise2.py
@@ -0,0 +1,9 @@
+# Write an if-else statement that assigns O to the variable b if the variable a is less than 10.
+# Otherwise, it should assign 99 to the variable b.
+
+A = 20
+if A < 10:
+    b = 0
+else:
+    b = 99
+print("value of b:",b)

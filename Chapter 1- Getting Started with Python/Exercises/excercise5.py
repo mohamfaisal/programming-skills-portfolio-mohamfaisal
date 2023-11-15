87df220c4369078e6f95b61d33aa6f3f11aca9ef
mohamfaisal <faisalferoz2005@gmail.com>
2023-11-14 17:23:09 -0800

    excercise 5

--- Chapter 1- Getting Started with Python/Exercises/excercise5.py
+++ Chapter 1- Getting Started with Python/Exercises/excercise5.py
@@ -1,4 +1,6 @@
+# write a python program which accepts the radius of a circle from the user and compute the area 
+
 import math
-radius = float(input("enter radius: "))
+radius = float(input("Enter your radius: "))
 area = math.pi * radius**2
 print(radius)
